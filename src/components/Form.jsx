c3afd3220223a2e39731b5e3594dfb2789b7a684
Your Name <you@example.com>
2024-07-23 23:35:16 -0300

    atualizacao modal

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,11 +10,11 @@ function Form({ addList, text, setText,valor,setValor }) {
             <section className="form">
                
                 <form onSubmit={addList}>
-                    <input className="valor" type="text" onChange={(e) => setValor(e.target.value)} value={valor} placeholder="valor..." />
+                    <input className="text" type="text" onChange={(e) => setValor(e.target.value)} value={valor} placeholder="valor..." />
                     <input className="text" type="text" onChange={(e) => setText(e.target.value)} value={text} placeholder="descrição..."/>
-                <button > add </button>
+                <button className="btn"> add </button>
                 </form>
-                <br />
+                
             </section>
 
         </>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,34 +1,5 @@
 import React from "react";
-
-const modal = {
-  position:'fixed',
-  top:'25%',
-  left:'50%',
-  transform:'translate(-50%, -50%',
-  backgroundColor:'#FFF',
-  padding:'50px',
-  zIndex:1000
-
-}
-const bgModal = {
-  position:'fixed',
-  top:0,
-  left:0,
-  right:0,
-  bottom:0,
-  backgroundColor:'rgba(0,0,0,.7)',
-  zIndex:1000
-
-}
-
-const btn={
-  color:'red',
-  backgroundColor:"white",
-  position:'relative',
-  left:'10rem'
-
-}
-
+import './Modal.css'
 
 
 function Modal({ open, children, onClose }) {
@@ -36,9 +7,9 @@ function Modal({ open, children, onClose }) {
 
   return (
     <>
-      <div style={bgModal} >
-        <div style={modal}>
-          <button style={btn} onClick={onClose}> X </button>
+      <div className='bgModal' >
+        <div className='modal'>
+          <button className='btn' onClick={onClose}> X </button>
           <p>obs: entrada sinal +  | saida sinal - </p> 
           {children}
         </div>
